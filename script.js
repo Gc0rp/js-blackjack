fc1a66b34ffdce8e5182ddbad3b6d4a3ab05fc0b
Gc0rp <ojasgupta4321@gmail.com>
2020-08-23 13:42:47 +1000

    Fixed display and score calculator

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ var playerScore = 0;
 
 var dealersOptions = ["hit", "stand"];
 
+var scoreCount = document.querySelectorAll(".score-count");
+
 prepareDeck.then(function getJSON(response) {
     const json = response.json();
     return json;
@@ -20,7 +22,13 @@ prepareDeck.then(function getJSON(response) {
     console.log(deckID);
 });
 
-function drawCard(selectedDeck, scoreForDeck) {
+
+// function displayDeck(scoreForDeck, player) {
+//     console.log(scoreCount[1].textContent + scoreForDeck);
+//     (player == "human") ? scoreCount[1].textContent = "Player Score : " + scoreForDeck : scoreCount[0].value = scoreForDeck;
+// }
+
+function drawCard(selectedDeck, scoreForDeck, player) {
     const drawCardURL = "https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=1"
 
     fetch(drawCardURL).then(function getJSON(response) {
@@ -34,20 +42,25 @@ function drawCard(selectedDeck, scoreForDeck) {
 
         // (Number(json.cards[0].value) === NaN) ? scoreForDeck = scoreForDeck + 10 : scoreForDeck = scoreForDeck + Number(json.cards[0].value);
 
-
-        json.cards[0].value.match(/[A-Z]/g) !== null ? console.log("Face card") : scoreForDeck = scoreForDeck + Number(json.cards[0].value)
+        (player === "human") ? scoreForDeck = 
+        json.cards[0].value.match(/[A-Z]/g) !== null ? scoreForDeck = scoreForDeck + 10 : scoreForDeck = scoreForDeck + Number(json.cards[0].value)
         
         selectedDeck.appendChild(img); 
 
-        console.log(selectedDeck + scoreForDeck);
+        console.log(scoreForDeck);       
     }); 
 }
+
 
 player.addEventListener("click", function(event){
     if(event.target.tagName === "BUTTON"){
         if(event.target.innerText === "Hit"){
             drawCard(playerDeck, playerScore);
+            console.log(playerScore);
+            // displayDeck(playerScore, "human");
+
             drawCard(dealersDeck, dealerScore);
+            // displayDeck(dealerScore, "AI");
         }
     }
 });
